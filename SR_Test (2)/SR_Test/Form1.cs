f02f0e05beffdf416e24c91e630b23baa9848b15
kandmoo <zetmp1500@gmail.com>
2019-06-04 19:49:41 +0900

    xml 수정

--- SR_Test (2)/SR_Test/Form1.cs
+++ SR_Test (2)/SR_Test/Form1.cs
@@ -102,7 +102,7 @@ namespace SR
                     }
                 };
             waveIn.StartRecording();
-            
+
             gapiTxtUpdate = true;
             _Speech_On.Play();
             gapiTimer = 5;
@@ -126,7 +126,7 @@ namespace SR
         static SoundPlayer _Speech_Sleep = new SoundPlayer(SR_Test.Properties.Resources.Speech_Sleep);
 
         const double _recognizeTimer = 5;
-        private List<orderStruct> orderList;
+        private List<orderClass> orderList;
         ProcessStartInfo cmd = new ProcessStartInfo();
         Process process = new Process();
         PowerShell ps = PowerShell.Create();
@@ -136,22 +136,15 @@ namespace SR
         bool isInorderList = false;
         static string richboxtextsave;
 
-        string[] inputdatalist;
-        string[] outputdatalist;
-
         public Form1()
         {
             InitializeComponent();
             initTTS();
-            orderList = new List<orderStruct>();
+            orderList = new List<orderClass>();
             isSTTActive = false;
             setting_set();
             cmdSetting();
 
-            orderStruct ost = new orderStruct();
-            ost.voiceInputs.Add("시리");
-            ost.voiceOutput = "명령을 말해주세요.";
-            orderList.Add(ost);
         }
         public void initRS()
         {
@@ -193,31 +186,7 @@ namespace SR
             if (tts.State != SynthesizerState.Ready) return;
             sttTimer = 3;
 
-            if (e.Result.Text == "시리")
-            {
-                inputmatch(sender, e);
-                return;
-            }
-
-            for (int i = 0; i < orderList.Count; i++)
-            {
-                if (!orderList[i].isInActive) continue;
-                for (int j = 0; j < orderList[i].voiceInputs.Count; j++)
-                {
-                    if (e.Result.Text == orderList[i].voiceInputs[j])
-                    {
-                        richTextBox1.Text = orderList[i].voiceOutput + "\n" + richTextBox1.Text;
-                        richTextBox2.Text = e.Result.Text + "\n" + richTextBox2.Text;
-                        tts.SpeakAsync(orderList[i].voiceOutput);
-                        //doPowerShellProgram(orderList[i].cmdString);
-                        cmdOrder(orderList[i].cmdString, orderList[i].args);
-                        isInorderList = true;
-                        break;
-                    }
-                }
-            }
-
-            if (isInorderList == false)
+            if (e.Result.Text == "썬데이" || e.Result.Text == "선데이")
             {
                 inputmatch(sender, e);
             }
@@ -226,11 +195,44 @@ namespace SR
         //
         private void inputmatch(object sender, SpeechRecognizedEventArgs e)
         {
-            tts.SpeakAsync("명령을 말씀해주세요.");
-
+            gapiTxt = "";
             StreamingMicRecognizeAsync(_recognizeTimer);
 
 
+            string inputString = gapiTxt;
+            orderUnit oU = new orderUnit();
+            bool brk = false;
+            foreach (orderClass it in orderList)
+            {
+                foreach (orderUnit it2 in it.units)
+                {
+                    if (compareOrder(inputString, it2.input))
+                    {
+                        oU = it2;
+                        brk = true;
+                        break;
+                    }
+                }
+                if (brk == true) break;
+            }
+
+            //mkPSOrder();
+            doPowerShellProgram(oU.PSOrder);
+        }
+        private bool compareOrder(string inputString, string order)
+        {
+            //var stems = TwitterKoreanProcessorCS.Stem(TwitterKoreanProcessorCS.Tokenize(str));
+            //List<string> inputTokens = new List<string>();
+            //foreach (KoreanToken it in stems)
+            //{
+            //    if (it.Pos == KoreanPos.Josa) continue;
+            //    inputTokens.Add(it.Text);
+            //}
+            //
+            return false;
+        }
+        private void mkPSOrder()
+        {
         }
         // 프로세스 실행
         private static void doProgram(string filename, string arg)
@@ -356,19 +358,21 @@ namespace SR
 
         }
 
+        //NotActive -> Active
         private void button4_Click(object sender, EventArgs e)
         {
             if (listBox1.SelectedItem == null) return;
             int count = listBox1.SelectedIndices.Count;
             for (int i = 0; i < count; i++)
             {
-                orderStruct temp = (orderStruct)listBox1.SelectedItem;
+                orderClass temp = (orderClass)listBox1.SelectedItem;
                 temp.isInActive = true;
                 listBox2.Items.Add(listBox1.SelectedItem);
                 listBox1.Items.Remove(listBox1.SelectedItem);
             }
         }
 
+        //Active -> NotActive
         private void button5_Click(object sender, EventArgs e)
         {
 
@@ -377,25 +381,26 @@ namespace SR
             int count = listBox2.SelectedIndices.Count;
             for (int i = 0; i < count; i++)
             {
-                orderStruct temp = (orderStruct)listBox2.SelectedItem;
+                orderClass temp = (orderClass)listBox2.SelectedItem;
                 temp.isInActive = false;
                 listBox1.Items.Add(listBox2.SelectedItem);
                 listBox2.Items.Remove(listBox2.SelectedItem);
             }
         }
 
+        //Delete Order
         private void button2_Click(object sender, EventArgs e)
         {
 
             if (listBox1.SelectedItem != null)
             {
-                orderStruct temp = (orderStruct)listBox1.SelectedItem;
+                orderClass temp = (orderClass)listBox1.SelectedItem;
                 listBox1.Items.Remove(listBox1.SelectedItem);
                 orderList.Remove(temp);
             }
             if (listBox2.SelectedItem != null)
             {
-                orderStruct temp = (orderStruct)listBox2.SelectedItem;
+                orderClass temp = (orderClass)listBox2.SelectedItem;
                 listBox2.Items.Remove(listBox2.SelectedItem);
                 orderList.Remove(temp);
             }
@@ -429,46 +434,48 @@ namespace SR
             }
             //grammarSet();
         }
-        private string orderRename(string str)
-        {
-            int count = 0;
-            string temp = str;
-            foreach (orderStruct it in orderList)
-            {
-                if (it.name == temp)
-                {
-                    count++;
-                    temp = str + "(" + count.ToString() + ")";
-                }
-            }
-            return temp;
-        }
         private void addOrder(string path)
         {
             XmlDocument xml = new XmlDocument();
             xml.Load(path);
-            orderStruct neworder;
+            orderClass neworder;
             XmlNodeList root = xml.SelectNodes("orders");
             foreach (XmlNode it in root)
             {
                 foreach (XmlNode it2 in it.SelectNodes("order"))
                 {
-                    neworder = new orderStruct();
-                    List<string> test = new List<string>();
-                    neworder.voiceInputs = new List<string>();
-                    neworder.name = orderRename(it2.SelectSingleNode("ordername").InnerText);
-                    neworder.voiceOutput = it2.SelectSingleNode("voiceOutput").InnerText;
-                    neworder.cmdString = it2.SelectSingleNode("cmdString").InnerText;
-                    neworder.description = it2.SelectSingleNode("description").InnerText;
-                    neworder.args = it2.SelectSingleNode("cmdArgs").InnerText;
-                    foreach (XmlNode it3 in it2.SelectSingleNode("inputOrders").SelectNodes("inputOrder"))
+                    neworder = new orderClass();
+                    neworder.name = it2.Attributes["name"].Value;
+                    neworder.description = it2.Attributes["description"].Value;
+                    neworder.isInActive = false;
+                    foreach(XmlNode it3 in it2.SelectNodes("orderUnit"))
                     {
-                        neworder.voiceInputs.Add(it3.InnerText);
+                        orderUnit newUnit = new orderUnit();
+
+                        //var
+                        string vars = it3.SelectSingleNode("var").InnerText;
+                        string[] var = vars.Split(' ', ',');
+                        foreach(string v in var)
+                        {
+                            newUnit.var.Add(v);
+                        }
+
+                        //input
+                        newUnit.input = it3.SelectSingleNode("input").InnerText;
+
+                        //psorder
+                        newUnit.PSOrder = it3.SelectSingleNode("PSOrder").InnerText;
+
+                        //voiceoutput
+                        newUnit.voiceOutput = it3.SelectSingleNode("voiceOutput").InnerText;
+
+                        neworder.units.Add(newUnit);
                     }
                     orderList.Add(neworder);
                     listBox1.Items.Add(orderList[orderList.Count - 1]);
                 }
             }
+            
         }
         private void deleteOrder(string name)
         {
@@ -484,59 +491,60 @@ namespace SR
             listBox1.Items.Add(listBox2.SelectedItem);
             listBox2.Items.Remove(listBox2.SelectedItem);
         }
-        private orderStruct findOrder(string name)
-        {
-            int i;
-            for (i = 0; i < orderList.Count; i++)
-            {
-                if (name == orderList[i].name)
-                {
-                    break;
-                }
-            }
-            return orderList[i];
-        }
-        private bool isThereOrder(string name)
-        {
-            foreach (orderStruct it in orderList)
-            {
-                if (name == it.name)
-                    return true;
-            }
-            return false;
-        }
+        
 
         private void saveSettingAsXml(string path)
         {
+
+
             XmlDocument xml = new XmlDocument();
             XmlNode root = xml.CreateElement("orders");
-            XmlNode order = xml.CreateElement("order"); ;
-            foreach (orderStruct it in orderList)
+            XmlNode order;
+
+            foreach (orderClass it in orderList)
             {
+                
                 order = xml.CreateElement("order");
-                XmlNode ordername = xml.CreateElement("ordername");
-                ordername.InnerText = it.name;
-                XmlNode voiceoutput = xml.CreateElement("voiceOutput");
-                voiceoutput.InnerText = it.voiceOutput;
-                XmlNode cmdstring = xml.CreateElement("cmdString");
-                cmdstring.InnerText = it.cmdString;
-                XmlNode args = xml.CreateElement("cmdArgs");
-                args.InnerText = it.args;
-                XmlNode description = xml.CreateElement("description");
-                description.InnerText = it.description;
-                XmlNode inputorders = xml.CreateElement("inputOrders");
-                foreach (string it2 in it.voiceInputs)
+                XmlAttribute nameattribute = xml.CreateAttribute("name");
+                nameattribute.Value = it.name;
+                XmlAttribute descriptionattribute = xml.CreateAttribute("description");
+                descriptionattribute.Value = it.description;
+
+                order.Attributes.Append(nameattribute);
+                order.Attributes.Append(descriptionattribute);
+                
+                foreach(orderUnit it2 in it.units)
                 {
-                    XmlNode inputorer = xml.CreateElement("inputOrder");
-                    inputorer.InnerText = it2;
-                    inputorders.AppendChild(inputorer);
+                    XmlNode orderUnit = xml.CreateElement("orderUnit");
+
+                    //var
+                    XmlNode var = xml.CreateElement("var");
+                    string vars = "";
+                    for(int x = 0; x < it2.var.Count;x++)
+                    {
+                        vars += it2.var[x];
+                        if (x != it2.var.Count - 1)
+                            vars += ",";
+                    }
+                    var.InnerText = vars;
+
+                    //input
+                    XmlNode input = xml.CreateElement("input");
+                    input.InnerText = it2.input;
+                    //output
+                    XmlNode psorder = xml.CreateElement("PSOrder");
+                    psorder.InnerText = it2.PSOrder;
+
+                    XmlNode voiceOutput = xml.CreateElement("voiceOutput");
+                    voiceOutput.InnerText = it2.voiceOutput;
+
+                    orderUnit.AppendChild(var);
+                    orderUnit.AppendChild(input);
+                    orderUnit.AppendChild(psorder);
+                    orderUnit.AppendChild(voiceOutput);
+
+                    order.AppendChild(orderUnit);
                 }
-                order.AppendChild(ordername);
-                order.AppendChild(voiceoutput);
-                order.AppendChild(cmdstring);
-                order.AppendChild(args);
-                order.AppendChild(description);
-                order.AppendChild(inputorders);
                 root.AppendChild(order);
             }
             xml.AppendChild(root);
@@ -544,8 +552,9 @@ namespace SR
         }
         private void grammarSet()
         {
-            string[] strs = new string[1];
-            strs[0] = "음성인식 실행";
+            string[] strs = new string[2];
+            strs[0] = "썬데이";
+            strs[1] = "선데이";
             GrammarBuilder gb = new GrammarBuilder(new Choices(strs));
             g = new Grammar(gb);
             initRS();
@@ -556,6 +565,7 @@ namespace SR
 
         }
 
+        //Save order setting
         private void button6_Click(object sender, EventArgs e)
         {
             if (orderList.Count == 0)
@@ -610,30 +620,31 @@ namespace SR
             
         }
 
+        //description button
         private void button7_Click(object sender, EventArgs e)
         {
             string str = "";
 
             if (listBox1.SelectedItem != null)
             {
-                orderStruct temp = (orderStruct)listBox1.SelectedItem;
+                orderClass temp = (orderClass)listBox1.SelectedItem;
                 str += temp.description + "\n\n";
                 str += "<실행 명령어>\n";
-                foreach (string it in temp.voiceInputs)
+                foreach (orderUnit it in temp.units)
                 {
-                    str += it + "\n";
+                    str += it.voiceOutput + "\n";
                 }
                 MessageBox.Show(str);
                 return;
             }
             else if (listBox2.SelectedItem != null)
             {
-                orderStruct temp = (orderStruct)listBox2.SelectedItem;
+                orderClass temp = (orderClass)listBox2.SelectedItem;
                 str += temp.description + "\n\n";
                 str += "<실행 명령어>\n";
-                foreach (string it in temp.voiceInputs)
+                foreach (orderUnit it in temp.units)
                 {
-                    str += it + "\n";
+                    str += it.input + "\n";
                 }
                 MessageBox.Show(str);
                 return;
@@ -651,62 +662,23 @@ namespace SR
         {
             gapiTxt = "";
             StreamingMicRecognizeAsync(_recognizeTimer);
+
+
         }
         
     }
-
-
-    class orderStruct
-    {
-        public List<string> voiceInputs;
-        public string voiceOutput;
-        public string cmdString;
-        public string description;
-        public string name;
-        public string args;
-        public bool isInActive;
-        public orderStruct(int x = 0)
-        {
-            voiceInputs = new List<string>();
-            voiceOutput = null;
-            cmdString = null;
-            description = null;
-            name = null;
-            isInActive = false;
-            args = null;
-        }
-        public override String ToString()
-        {
-            return this.name;
-        }
-        public string ListBoxDisplayValue
-        {
-            get
-            {
-                return this.name;
-            }
-        }
-        public string DisplayValue
-        {
-            get
-            {
-                return this.description;
-            }
-        }
-    }
-
+    
     class orderUnit
     {
         public List<string> var = new List<string>();
         public string input = "";
         public string PSOrder = "";
-        public string PSReturn = "";
         public string voiceOutput = "";
-        orderUnit(string input, string PSOrder, string PSReturn, string voiceOutput)
+        public orderUnit() { }
+        public orderUnit(string input, string PSOrder, string voiceOutput)
         {
             this.input = input;
             this.PSOrder = PSOrder;
-            this.PSReturn = PSReturn;
             this.voiceOutput = voiceOutput;
         }
     }
@@ -714,6 +686,7 @@ namespace SR
     {
         public string name = "";
         public string description = "";
+        public bool isInActive = false;
         public List<orderUnit> units = new List<orderUnit>();
         public override String ToString()
         {
